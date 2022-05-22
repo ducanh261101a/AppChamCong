8c7a95e66e57119f75f6d589d759bf8cdcf4e573
thaiducanh <thaianh19020208@gmail.com>
2022-05-22 17:51:33 +0700

    fix bug UI

--- src/Screens/DanhSachDangKiVaoRaScreen/DanhSachDangKiVaoRaScreen.js
+++ src/Screens/DanhSachDangKiVaoRaScreen/DanhSachDangKiVaoRaScreen.js
@@ -1,68 +1,59 @@
 import React from 'react';
-import {
-    Button,
-    GeneralLayout,
-    // TabPanel,
-    Wrapper
-} from '../../Components';
+import {Button, GeneralLayout, TabPanel, Wrapper} from '../../Components';
 import styles from './styles';
 import {
-    DANHSACHDANGKIVAORASCREEN_RIGHT_TITLE,
-    DANHSACHDANGKIVAORASCREEN_TITLE
+  DANHSACHDANGKIVAORASCREEN_RIGHT_TITLE,
+  DANHSACHDANGKIVAORASCREEN_TITLE,
 } from '../../Shared/text';
-import { useEmployeeRequest } from './services';
+import {useEmployeeRequest} from './services';
 
-export default function DanhSachDangKiVaoRaScreen({
-    navigation
-}) {
+export default function DanhSachDangKiVaoRaScreen({navigation}) {
+  // const {
+  //     goOutRequestWaitingForApproval,
+  //     goOutRequestApproved,
+  //     goOutRequestRejected
+  // } = useEmployeeRequest()
 
-    // const {
-    //     goOutRequestWaitingForApproval,
-    //     goOutRequestApproved,
-    //     goOutRequestRejected
-    // } = useEmployeeRequest()
+  return (
+    <Wrapper>
+      <GeneralLayout
+        headerLeftTitle={DANHSACHDANGKIVAORASCREEN_TITLE}
+        headerRight={{
+          type: 'string',
+          title: DANHSACHDANGKIVAORASCREEN_RIGHT_TITLE,
+          onPress: () => navigation.navigate('LichSuChamCongScreen'),
+        }}
+        bodyStyle={styles.body}
+        hasBackgroundBody={true}>
+        <TabPanel
+          defaultIndex={1}
+          headerItems={[
+            {
+              index: 1,
+              label: 'Chưa duyệt',
+              quantity: 0,
+              content: [],
+            },
+            {
+              index: 2,
+              label: 'Đã duyệt',
+              quantity: 0,
+              content: [],
+            },
+            {
+              index: 3,
+              label: 'Từ chối duyệt',
+              quantity: 0,
+              content: [],
+            },
+          ]}
+        />
 
-    return (
-        <Wrapper>
-            <GeneralLayout
-                headerLeftTitle={DANHSACHDANGKIVAORASCREEN_TITLE}
-                headerRight={{
-                    type: "string",
-                    title: DANHSACHDANGKIVAORASCREEN_RIGHT_TITLE,
-                    onPress: () => navigation.navigate('LichSuChamCongScreen')
-                }}
-                bodyStyle={styles.body}
-                hasBackgroundBody={true}
-            >
-                {/* <TabPanel
-                    defaultIndex={1}
-                    headerItems={[
-                        {
-                            index: 1,
-                            label: 'Chưa duyệt',
-                            quantity: goOutRequestWaitingForApproval?.length,
-                            content: goOutRequestWaitingForApproval
-                        },
-                        {
-                            index: 2,
-                            label: 'Đã duyệt',
-                            quantity: goOutRequestApproved?.length,
-                            content: goOutRequestApproved
-                        }, {
-                            index: 3,
-                            label: 'Từ chối duyệt',
-                            quantity: goOutRequestRejected?.length,
-                            content: goOutRequestRejected
-                        }
-                    ]}
-                /> */}
-
-
-                <Button
-                    type='add'
-                    onPress={() => navigation.navigate('DangKiVaoRaScreen')}
-                />
-            </GeneralLayout>
-        </Wrapper >
-    );
+        <Button
+          type="add"
+          onPress={() => navigation.navigate('DangKiVaoRaScreen')}
+        />
+      </GeneralLayout>
+    </Wrapper>
+  );
 }
